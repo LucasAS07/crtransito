81cbd09104a5f6a5c42a9c289b1f909243ebd177
Lucas Rodrigues de Oliveira <lucas-oliveira25@outlook.com>
2025-01-05 10:42:39 -0300

    Criação e configuração do projeto

---
+++ src/main/java/io/lrsystem/crtransito/CrtransitoApiApplication.java
@@ -0,0 +1,13 @@
+package io.lrsystem.crtransito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrtransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrtransitoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lrsystem/crtransito/CrtransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lrsystem.crtransito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrtransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
